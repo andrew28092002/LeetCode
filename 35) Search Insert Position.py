b1ded10b12481c9489f4d88ee5de17fb06cd3664
andrew28092002 <andreiastanaev2002@gmail.com>
2022-05-19 11:43:44 +0500

    53) 1

--- 35) Search Insert Position.py
+++ 35) Search Insert Position.py
@@ -1,7 +1,6 @@
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
         if target > nums[-1]: return len(nums)
-        if target < nums[0]: return 0
         left = 0
         right = len(nums)-1
         while right >= left:
